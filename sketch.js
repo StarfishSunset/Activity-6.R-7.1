a27c61cbf1c8ddeb755c2c66c9a00ac930d096d2
StarfishSunset <72940354+StarfishSunset@users.noreply.github.com>
2020-10-26 01:12:59 +0800

    horizontal (one ball :( )

--- sketch.js
+++ sketch.js
@@ -5,32 +5,31 @@ function setup() {
 
 for (let i = 0; i < 2; i++) {
   balln[i] = {
-      x: random(width),
-      y: random(50),
-      dy: 0,
+      x: random(50),
+      y: random(height),
+      dx: 0,
     }
 }
 }
 
-
 function draw() {
   background(200);
 
-  balln[0].y += balln[0].dy;
-  balln[0].dy += 0.25;
+  balln[0].x += balln[0].dy;
+  balln[0].dx += 0.25;
   circle(balln[0].x, balln[0].y, 50);
 
-  balln[1].y += balln[1].dy;
-  balln[1].dy += 0.25;
+  balln[1].x += balln[1].dx;
+  balln[1].dx += 0.25;
   circle(balln[1].x, balln[1].y, 50);
 
 
   // if the object hits the bottom of the canvas
   // make it go up instead of down
-  if (balln[0].y >= height) {
-    balln[0].dy = -0.95 * balln[0].dy;
+  if (balln[0].x >= width) {
+    balln[0].dx = -0.95 * balln[0].dx;
   }
-  if (balln[1].y >= height) {
-    balln[1].dy = -0.95 * balln[1].dy;
+  if (balln[1].x >= width) {
+    balln[1].dx = -0.95 * balln[1].dx;
   }
 }
