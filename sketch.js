a8001f0c1a8812b7795644324e956559c8bde125
StarfishSunset <72940354+StarfishSunset@users.noreply.github.com>
2020-10-26 00:57:04 +0800

    array created

--- sketch.js
+++ sketch.js
@@ -1,32 +1,36 @@
-let ball0;
-let ball1;
+var balln =[];
+
 function setup() {
-  createCanvas(windowWidth, windowHeight);
-  ball0 = {
-    x: random(width),
-    y: random(50),
-    dy: 0,
-  }
-  ball1 = {
-    x: random(width),
-    y: random(50),
-    dy: 0,
-  }
+  createCanvas(400, 400);
+
+for (let i = 0; i < 2; i++) {
+  balln[i] = {
+      x: random(width),
+      y: random(50),
+      dy: 0,
+    }
 }
+}
+
+
 function draw() {
   background(200);
-  ball0.y += ball0.dy;
-  ball0.dy += 0.25;
-  circle(ball0.x, ball0.y, 50);
-  ball1.y += ball1.dy;
-  ball1.dy += 0.25;
-  circle(ball1.x, ball1.y, 50);
+
+  balln[0].y += balln[0].dy;
+  balln[0].dy += 0.25;
+  circle(balln[0].x, balln[0].y, 50);
+
+  balln[1].y += balln[1].dy;
+  balln[1].dy += 0.25;
+  circle(balln[1].x, balln[1].y, 50);
+
+
   // if the object hits the bottom of the canvas
   // make it go up instead of down
-  if (ball0.y >= height) {
-    ball0.dy = -0.95 * ball0.dy;
+  if (balln[0].y >= height) {
+    balln[0].dy = -0.95 * balln[0].dy;
   }
-  if (ball1.y >= height) {
-    ball1.dy = -0.95 * ball1.dy;
+  if (balln[1].y >= height) {
+    balln[1].dy = -0.95 * balln[1].dy;
   }
 }
