e43bb68121e96fe3425e790d90a7c9b033b077fe
StarfishSunset <72940354+StarfishSunset@users.noreply.github.com>
2020-10-26 01:38:10 +0800

    color added

--- sketch.js
+++ sketch.js
@@ -3,15 +3,19 @@ var balln =[];
 function setup() {
   createCanvas(400, 400);
 
+  let r = random(0, 255);
+  let g =random(0, 255);
+  let b = random(0, 255);
+
 for (let i = 0; i < 2; i++) {
   balln[i] = {
-      x: random(50),
+      x: random(20),
       y: random(height),
-      dx: 0,
+      dx: random(-3, 3),
     }
+    fill(r, g, b)
 }
 }
-
 function draw() {
   background(200);
 
@@ -21,11 +25,11 @@ function draw() {
 
   balln[1].x += balln[1].dx;
   balln[1].dx += 0.25;
-  circle(balln[1].x, balln[1].y, 50);
-
+  rect(balln[1].x, balln[1].y, 50);
 
   // if the object hits the bottom of the canvas
   // make it go up instead of down
+
   if (balln[0].x >= width) {
     balln[0].dx = -0.95 * balln[0].dx;
   }
